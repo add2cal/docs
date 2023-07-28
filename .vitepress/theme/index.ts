c6b4c694aa047b8948cdbda75cedcc767c59454e
Jens Kuerschner <mail@jenskuerschner.de>
2023-07-28 19:24:04 +0200

    updates and lang cookie

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,19 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './custom-style.css'
+import { h, watchEffect } from 'vue';
+import { useData, inBrowser } from 'vitepress';
+import Theme from 'vitepress/theme';
+import './custom-style.css';
 
 export default {
   ...Theme,
+  setup() {
+    const { lang } = useData();
+    watchEffect(() => {
+      if (inBrowser) {
+        document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2024 00:00:00 UTC; path=/`
+      }
+    })
+  },
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
